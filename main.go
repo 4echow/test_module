b71b51a90a203cf54df1b6774f631663ebae975a
aantoni <aantoni@intevation.de>
2025-05-14 15:19:38 +0200

    add godoc

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+// Add adds two Integer numbers and returns their sum
+//
+// More info on the addition operation on [Mathisfun]
+// [Mathisfun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
